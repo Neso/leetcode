79b2232194958ff6de02ce2e7f69c2cab9433eac
Nenad <Neso@users.noreply.github.com>
2024-03-12 13:25:45 +0000

    Create 1171 remove zero sum consecutive nodes from linked list.ts

---
+++ daily question/1171 remove zero sum consecutive nodes from linked list.ts
@@ -0,0 +1,119 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+interface ListNode {
+    val: number;
+    next: ListNode | null;
+    previous: ListNode | null;
+}
+
+function removeZeroSumSublists(head: ListNode | null): ListNode | null {
+    let current = head;
+    let previous: ListNode | null = null;
+
+    while (current?.val !== 0) {
+        current = current?.next || null;
+    }
+
+    while (current) {
+        current.previous = previous;
+        previous = current;
+        current = current.next;
+
+        if (current?.val === 0) {
+            previous.next = current.next;
+            current = previous;
+        }
+    }
+
+    return checkList(head);
+};
+
+function checkList(head: ListNode | null): ListNode | null {
+    if (!head) {
+        return head;
+    }
+
+    let startCount = head;
+    let count = head?.val || 0;
+
+    let sumLists: {[key: number]: ListNode} = {};
+
+    let current: ListNode | null = head;
+
+    let newList: ListNode | null = null;
+
+    let aboveZero = count > 0;
+
+    if (!sumLists[count]) {
+        sumLists[count] = current;
+    }
+
+    let isListChanged = false;
+
+    do {
+        while (current) {
+            current = current.next;
+            count += current?.val || 0;
+
+            if (!current) {
+                break;
+            }
+
+            if (current?.val > 0 && aboveZero) {
+                if (!sumLists[count]) {
+                    sumLists[count] = current;
+                }
+            } else if (current?.val < 0 && !aboveZero) {
+                if (!sumLists[count]) {
+                    sumLists[count] = current;
+                }
+            } else {
+                if (sumLists[count]) {
+                    const countHit = sumLists[count];
+                    delete sumLists[count];
+
+                    if (newList) {
+                        newList.next = current?.next;
+
+                        if (current?.next) {
+                            current.next.previous = newList;
+                        }
+                    } else {
+                        newList = countHit.previous;
+
+                        if (newList) {
+                            newList.next = current?.next;
+                        } else {
+                            newList = current?.next;
+                        }
+                    }
+
+                    isListChanged = true;
+                }
+            }
+        }
+
+        current = newList;
+        sumLists = {};
+        count = current?.val || 0;
+        aboveZero = count > 0;
+        newList = null;
+    } while (isListChanged);
+
+    if (newList) {
+        return newList;
+    }
+
+    return head;
+
+}
